0a9b5eee577410564b425de3aea871c4697497ea
Pablo Fernandez <contact@pablofernandez.com>
2016-05-03 16:52:17 +0600

    Added files via upload

---
+++ jcart/config.php
@@ -0,0 +1,85 @@
+<?php
+// Do NOT store any sensitive info in this file!!!
+// loaded into the browser as plain text via Ajax
+////////////////////////////////////////////////////////////////////////////////
+// REQUIRED SETTINGS
+
+// Path to your jcart files
+$config['jcartPath']              = 'jcart/';
+
+// Path to your checkout page
+$config['checkoutPath']           = 'checkout.php';
+
+// The HTML name attributes used in your item forms
+$config['item']['id']             = 'my-item-id';    // Item id
+$config['item']['name']           = 'my-item-name';    // Item name
+$config['item']['price']          = 'my-item-price';    // Item price
+$config['item']['qty']            = 'my-item-qty';    // Item quantity
+$config['item']['url']            = 'my-item-url';    // Item URL (optional)
+$config['item']['add']            = 'my-add-button';    // Add to cart button
+
+// PayPal secure merchant ID
+$config['paypal']['id']           = 'pay@swiftrover.com';
+
+////////////////////////////////////////////////////////////////////////////////
+// OPTIONAL SETTINGS
+
+// Three-letter currency code, defaults to USD if empty
+// See available options here: http://j.mp/agNsTx
+$config['currencyCode']           = '';
+
+// Add a unique token to form posts to prevent CSRF exploits
+// Learn more: http://conceptlogic.com/jcart/security.php
+$config['csrfToken']              = false;
+
+// Override default cart text
+$config['text']['cartTitle']      = '';    // Shopping Cart
+$config['text']['singleItem']     = '';    // Item
+$config['text']['multipleItems']  = '';    // Items
+$config['text']['subtotal']       = '';    // Subtotal
+$config['text']['update']         = '';    // update
+$config['text']['checkout']       = '';    // checkout
+$config['text']['checkoutPaypal'] = '';    // Checkout with PayPal
+$config['text']['removeLink']     = '';    // remove
+$config['text']['emptyButton']    = '';    // empty
+$config['text']['emptyMessage']   = '';    // Your cart is empty!
+$config['text']['itemAdded']      = '';    // Item added!
+$config['text']['priceError']     = '';    // Invalid price format!
+$config['text']['quantityError']  = '';    // Item quantities must be whole numbers!
+$config['text']['checkoutError']  = '';    // Your order could not be processed!
+
+// Override the default buttons by entering paths to your button images
+$config['button']['checkout']     = '';
+$config['button']['paypal']       = '';
+$config['button']['update']       = '';
+$config['button']['empty']        = '';
+
+
+////////////////////////////////////////////////////////////////////////////////
+// ADVANCED SETTINGS
+
+// Display tooltip after the visitor adds an item to their cart?
+$config['tooltip']                = true;
+
+// Allow decimals in item quantities?
+$config['decimalQtys']            = false;
+
+// How many decimal places are allowed?
+$config['decimalPlaces']          = 1;
+
+// Number format for prices, see: http://php.net/manual/en/function.number-format.php
+$config['priceFormat']            = array('decimals' => 2, 'dec_point' => '.', 'thousands_sep' => ',');
+
+// Send visitor to PayPal via HTTPS?
+$config['paypal']['https']        = true;
+
+// Use PayPal sandbox?
+$config['paypal']['sandbox']      = false;
+
+// The URL a visitor is returned to after completing their PayPal transaction
+$config['paypal']['returnUrl']    = '';
+
+// The URL of your PayPal IPN script
+$config['paypal']['notifyUrl']    = '';
+
+?>
