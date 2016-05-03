e4ddaa7fd94527caf0c0c327b9d1450ce3ee8258
Pablo Fernandez <contact@pablofernandez.com>
2016-05-03 03:54:12 -0700

    Update config.php

--- jcart/config.php
+++ jcart/config.php
@@ -4,13 +4,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 // REQUIRED SETTINGS
 
-// Path to your jcart files
+// Path to  jcart files
 $config['jcartPath']              = 'jcart/';
 
-// Path to your checkout page
+// Path to  checkout page
 $config['checkoutPath']           = 'checkout.php';
 
-// The HTML name attributes used in your item forms
 $config['item']['id']             = 'my-item-id';    // Item id
 $config['item']['name']           = 'my-item-name';    // Item name
 $config['item']['price']          = 'my-item-price';    // Item price
@@ -22,14 +21,10 @@ $config['item']['add']            = 'my-add-button';    // Add to cart button
 $config['paypal']['id']           = 'pay@swiftrover.com';
 
 ////////////////////////////////////////////////////////////////////////////////
-// OPTIONAL SETTINGS
 
-// Three-letter currency code, defaults to USD if empty
-// See available options here: http://j.mp/agNsTx
 $config['currencyCode']           = '';
 
-// Add a unique token to form posts to prevent CSRF exploits
-// Learn more: http://conceptlogic.com/jcart/security.php
+//unique token to form posts to prevent CSRF exploits
 $config['csrfToken']              = false;
 
 // Override default cart text
@@ -48,7 +43,7 @@ $config['text']['priceError']     = '';    // Invalid price format!
 $config['text']['quantityError']  = '';    // Item quantities must be whole numbers!
 $config['text']['checkoutError']  = '';    // Your order could not be processed!
 
-// Override the default buttons by entering paths to your button images
+//OR BUTTON IMAGES
 $config['button']['checkout']     = '';
 $config['button']['paypal']       = '';
 $config['button']['update']       = '';
@@ -56,7 +51,6 @@ $config['button']['empty']        = '';
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// ADVANCED SETTINGS
 
 // Display tooltip after the visitor adds an item to their cart?
 $config['tooltip']                = true;
@@ -67,7 +61,7 @@ $config['decimalQtys']            = false;
 // How many decimal places are allowed?
 $config['decimalPlaces']          = 1;
 
-// Number format for prices, see: http://php.net/manual/en/function.number-format.php
+// Number format for prices
 $config['priceFormat']            = array('decimals' => 2, 'dec_point' => '.', 'thousands_sep' => ',');
 
 // Send visitor to PayPal via HTTPS?
